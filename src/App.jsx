862dff1e564ff46930412bc9c439ef75fea72f60
Esteban Sepulveda <tebansd@icloud.com>
2023-09-29 20:41:35 -0700

    added some styles to page using bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import TopBar from './Components/TopBar'
+import Background from './Components/Background'
+import StoreListings from './Components/StoreListings'
+
+
+function App() {
+  return (
+    <div className="App">
+      <TopBar />
+      <Background />
+      <StoreListings />
+    </div>
+  );
+}
+
+export default App;
